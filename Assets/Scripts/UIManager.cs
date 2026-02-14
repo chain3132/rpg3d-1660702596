bad1fd9d699d7ac2fc5347f8a795393cef3f7ac4
chain3132 <148683922+chain3132@users.noreply.github.com>
2026-02-14 10:06:04 +0700

    togglePauseUnpause

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private RectTransform selectionBox;
+    [SerializeField] private Toggle togglePauseUnpause;
     public RectTransform SelectionBox
     {
         get { return selectionBox; }
@@ -16,16 +18,41 @@ public class UIManager : MonoBehaviour
         instance = this;
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            togglePauseUnpause.isOn = !togglePauseUnpause.isOn;
+        }
+    }
+
     public void ToggleAI(bool isOn)
     {
         foreach (var member in PartyManager.instance.Members)
         {
             AttackAI ai = member.gameObject.GetComponent<AttackAI>();
-            Debug.Log(ai.gameObject.name);
             if (ai != null)
             {
                 ai.enabled = isOn;
                 Debug.Log(ai.enabled);
+            }
+        }
+    }
+
+    public void PauseUpdate(bool isOn)
+    {
+        Time.timeScale = togglePauseUnpause.isOn ? 0 : 1;
+    }
+    public void SelectAll()
+    {
+        PartyManager.instance.SelectChars.Clear();
+
+        foreach (Characters member in PartyManager.instance.Members)
+        {
+            if (member.CurHP > 0)
+            {
+                member.ToggleRingSelection(true);
+                PartyManager.instance.SelectChars.Add(member);
             }
         }
     }
